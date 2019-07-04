2ce521cba32406ed6962917ba2ea7efc8e6fe230
Matthew Blott <matthew.blott@gmail>
2019-07-04 17:27:29 +0100

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace aspnet_template
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddCloudscribeNavigation(Configuration.GetSection("NavigationOptions"));
+      services.AddMvc()
+        .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+        .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseStaticFiles();
+      app.UseMvcWithDefaultRoute();
+
+    }
+    
+  }
+  
+}
---
+++ controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_template.controllers
+{
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult About()
+    {
+      return View();
+    }
+    
+  }
+    
+}
